96ceb828751b447129e8b951db2e22145801b546
adaickalavan <adaickalavan@gmail.com>
2020-03-26 02:46:17 +0800

    Update

--- app/greeter_server.cc
+++ app/greeter_server.cc
@@ -46,6 +46,10 @@ class GreeterServiceImpl final : public Greeter::Service {
     reply->set_message(prefix + request->name());
     return Status::OK;
   }
+  Status SayHelloAgain(ServerContext* context, const HelloRequest* request,
+                        HelloReply* ){
+
+  }
 };
 
 void RunServer() {
