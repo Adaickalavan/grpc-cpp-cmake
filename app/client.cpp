82c9641d98cc2538bfc9817cd4a940970c1ada87
adaickalavan <adaickalavan@gmail.com>
2020-04-07 11:14:51 +0800

    Update

--- app/client.cpp
+++ app/client.cpp
@@ -33,125 +33,128 @@ ostream& operator<<(ostream& os, const vector<T>& v){
 // Performs an HTTP GET and prints the response
 int main(int argc, char** argv)
 {
-    try
-    {
-        // Check command line arguments.
-        if(argc != 4 && argc != 5)
-        {
-            std::cerr <<
-                "Usage: http-client-sync <host> <port> <target> [<HTTP version: 1.0 or 1.1(default)>]\n" <<
-                "Example:\n" <<
-                "    http-client-sync www.example.com 80 /\n" <<
-                "    http-client-sync www.example.com 80 / 1.0\n";
-            return EXIT_FAILURE;
-        }
-        auto const host = argv[1];
-        auto const port = argv[2];
-        auto const target = argv[3];
-        cout << argv[1] <<" - " << argv[2] << " - "<< argv[3] << "\n";
-        // return 1;
-        int version = argc == 5 && !std::strcmp("1.0", argv[4]) ? 10 : 11;
 
-        // The io_context is required for all I/O
-        net::io_context ioc;
+    
 
-        // These objects perform our I/O
-        tcp::resolver resolver(ioc);
-        beast::tcp_stream stream(ioc);
+    // try
+    // {
+    //     // Check command line arguments.
+    //     if(argc != 4 && argc != 5)
+    //     {
+    //         std::cerr <<
+    //             "Usage: http-client-sync <host> <port> <target> [<HTTP version: 1.0 or 1.1(default)>]\n" <<
+    //             "Example:\n" <<
+    //             "    http-client-sync www.example.com 80 /\n" <<
+    //             "    http-client-sync www.example.com 80 / 1.0\n";
+    //         return EXIT_FAILURE;
+    //     }
+    //     auto const host = argv[1];
+    //     auto const port = argv[2];
+    //     auto const target = argv[3];
+    //     cout << argv[1] <<" - " << argv[2] << " - "<< argv[3] << "\n";
+    //     // return 1;
+    //     int version = argc == 5 && !std::strcmp("1.0", argv[4]) ? 10 : 11;
 
-        // Look up the domain name
-        auto const results = resolver.resolve(host,port);
+    //     // The io_context is required for all I/O
+    //     net::io_context ioc;
 
-        // Make the connection on the IP address we get from a lookup
-        stream.connect(results);
+    //     // These objects perform our I/O
+    //     tcp::resolver resolver(ioc);
+    //     beast::tcp_stream stream(ioc);
 
-        // Set up an HTTP POST request message
-        http::request<http::string_body> req{http::verb::post, target, version};
-        req.set(http::field::host, host);
-        req.set(beast::http::field::content_type, "application/json; charset=utf-8");
+    //     // Look up the domain name
+    //     auto const results = resolver.resolve(host,port);
 
-        // POST JSON message
-        // {
-        //     "signature_name" : "serving_default",
-        //     "instances" : [                
-        //         {
-		// 		       "image": 
-        //                 {
-        //                     "b64": "aW1hZ2UgYnl0ZXM=",
-        //                 }
-        //         },
-        //     ]
-		// }
-        // Form JSON message
-        pt::ptree request;
-        request.put("signature_name", "serving_default");
-        pt::ptree image;
-        image.put("b64", "aW1hZ2UgYnl0ZXM=");
-        pt::ptree instance_node;
-        instance_node.add_child("image",image);      
-        pt::ptree instances_node;
-        instances_node.push_back(std::make_pair("", instance_node));
-        request.add_child("instances", instances_node);
+    //     // Make the connection on the IP address we get from a lookup
+    //     stream.connect(results);
 
-        // Print the generated JSON
-        pt::write_json(cout, request);
+    //     // Set up an HTTP POST request message
+    //     http::request<http::string_body> req{http::verb::post, target, version};
+    //     req.set(http::field::host, host);
+    //     req.set(beast::http::field::content_type, "application/json; charset=utf-8");
 
-        // Send the HTTP request to the remote host
-        http::write(stream, req);
+    //     // POST JSON message
+    //     // {
+    //     //     "signature_name" : "serving_default",
+    //     //     "instances" : [                
+    //     //         {
+	// 	// 		       "image": 
+    //     //                 {
+    //     //                     "b64": "aW1hZ2UgYnl0ZXM=",
+    //     //                 }
+    //     //         },
+    //     //     ]
+	// 	// }
+    //     // Form JSON message
+    //     pt::ptree request;
+    //     request.put("signature_name", "serving_default");
+    //     pt::ptree image;
+    //     image.put("b64", "aW1hZ2UgYnl0ZXM=");
+    //     pt::ptree instance_node;
+    //     instance_node.add_child("image",image);      
+    //     pt::ptree instances_node;
+    //     instances_node.push_back(std::make_pair("", instance_node));
+    //     request.add_child("instances", instances_node);
 
-        // This buffer is used for reading and must be persisted
-        beast::flat_buffer buffer;
+    //     // Print the generated JSON
+    //     pt::write_json(cout, request);
 
-        // Declare a container to hold the response
-        http::response<http::string_body> res;
+    //     // Send the HTTP request to the remote host
+    //     http::write(stream, req);
 
-        // Receive the HTTP response
-        http::read(stream, buffer, res);
+    //     // This buffer is used for reading and must be persisted
+    //     beast::flat_buffer buffer;
 
-        // Response JSON message
-        // {
-        //     "predictions":[
-        //         {
-        //             "classes": int,
-        //             "probabilities": int[],
-        //         },
-        //     ]
-        // }       
-        // Parse response
-        std::istringstream istrstream(res.body());
-        pt::ptree response;
-        pt::read_json(istrstream, response);
-        vector<std::pair<int,vector<float>>> predictions;
-        for (pt::ptree::value_type &prediction : response.get_child("predictions")){
-            int classes = prediction.second.get<int>("classes");
+    //     // Declare a container to hold the response
+    //     http::response<http::string_body> res;
 
-            vector<float> prob;
-            for (pt::ptree::value_type &probabilities : prediction.second.get_child("probabilities")){
-                prob.push_back(probabilities.second.get_value<float>());
-            }
+    //     // Receive the HTTP response
+    //     http::read(stream, buffer, res);
+
+    //     // Response JSON message
+    //     // {
+    //     //     "predictions":[
+    //     //         {
+    //     //             "classes": int,
+    //     //             "probabilities": int[],
+    //     //         },
+    //     //     ]
+    //     // }       
+    //     // Parse response
+    //     std::istringstream istrstream(res.body());
+    //     pt::ptree response;
+    //     pt::read_json(istrstream, response);
+    //     vector<std::pair<int,vector<float>>> predictions;
+    //     for (pt::ptree::value_type &prediction : response.get_child("predictions")){
+    //         int classes = prediction.second.get<int>("classes");
+
+    //         vector<float> prob;
+    //         for (pt::ptree::value_type &probabilities : prediction.second.get_child("probabilities")){
+    //             prob.push_back(probabilities.second.get_value<float>());
+    //         }
             
-            predictions.push_back(std::pair(classes, prob));
-        }
+    //         predictions.push_back(std::pair(classes, prob));
+    //     }
 
-        // Print response partially
-        pt::write_json(cout, response);
-        cout << predictions[0].first << " -- " << predictions[0].second << "\n";
+    //     // Print response partially
+    //     pt::write_json(cout, response);
+    //     cout << predictions[0].first << " -- " << predictions[0].second << "\n";
 
-        // Gracefully close the socket
-        beast::error_code ec;
-        stream.socket().shutdown(tcp::socket::shutdown_both, ec);
+    //     // Gracefully close the socket
+    //     beast::error_code ec;
+    //     stream.socket().shutdown(tcp::socket::shutdown_both, ec);
 
-        // not_connected happens sometimes
-        // so don't bother reporting it.
-        if(ec && ec != beast::errc::not_connected)
-            throw beast::system_error{ec};
+    //     // not_connected happens sometimes
+    //     // so don't bother reporting it.
+    //     if(ec && ec != beast::errc::not_connected)
+    //         throw beast::system_error{ec};
 
-        // If we get here then the connection is closed gracefully
-    }
-    catch(std::exception const& e)
-    {
-        std::cerr << "Error: " << e.what() << std::endl;
-        return EXIT_FAILURE;
-    }
+    //     // If we get here then the connection is closed gracefully
+    // }
+    // catch(std::exception const& e)
+    // {
+    //     std::cerr << "Error: " << e.what() << std::endl;
+    //     return EXIT_FAILURE;
+    // }
     return EXIT_SUCCESS;
 }
